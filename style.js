34129b6d0144acb5ad8bef994f49c8fe8f680d50
Grizzly <grizzlyxyz@gmail>
2019-01-10 14:32:00 +0100

    exofonction

---
+++ style.js
@@ -0,0 +1,37 @@
+// Exo 1
+
+let phrase = "J’aime les tartines et j’en mange";
+
+console.log(phrase);
+
+let phrases2 = "4 par jours";
+console.log(phrase + phrases2);
+
+let nombre = 5; 
+let nombre2 = 35;
+console.log =(nombre + nombre2);
+
+console.log=(phrase + nombre);
+
+// Exo 2
+
+let hello = "Salut tout le monde";
+console.log(hello);
+let goodbye = " Aurevoir ! ";
+console.log(goodbye);
+
+hello = goodbye;
+
+console.log(hello);
+
+//Exo 3
+
+let number1 = "4";
+let number2 = "6";
+
+console.log(number1 > number2);
+console.log(number < number2);
+console.log(number1 == number2);
+console.log(number1 != number2);
+
+
